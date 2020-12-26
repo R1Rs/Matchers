0d51db0f130498462c0dcc2d7207a4033336e766
Renat <suleymanov_renat@mail.ru>
2020-12-26 20:46:30 +0300

    Правки

---
+++ src/js/__test__/functionArray.test.js
@@ -0,0 +1,35 @@
+import arraySorting from "../functionArray";
+
+test("test function sorting", () => {
+  const array = [
+    { name: 'мечник', health: 10 },
+    { name: 'маг', health: 100 },
+    { name: 'лучник', health: 80 },
+  ];
+
+  const received = arraySorting(array);
+  const expected = [
+    { name: 'маг', health: 100 },
+    { name: 'лучник', health: 80 },
+    { name: 'мечник', health: 10 },
+  ];
+
+  expect(received).toEqual(expected);
+});
+
+test("test function sorting", () => {
+  const array = [
+    { name: 'мечник', health: 10 },
+    { name: 'маг', health: 100 },
+    { name: 'лучник', health: 80 },
+  ];
+
+  const received = arraySorting(array);
+  const expected = [
+    { name: 'маг', health: 100 },
+    { name: 'лучник', health: 80 },
+    { name: 'мечник', health: 10 },
+  ];
+
+  expect(received).toBe(expected);
+});
